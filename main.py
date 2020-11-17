2aa1a0e32dcd42c175e76686a4d9f427d0c67638
João Paludo <jas.paludo@gmail.com>
2020-11-17 18:14:47 -0300

    Add main entry point

---
+++ main.py
@@ -0,0 +1,42 @@
+import pygame
+
+from src.Planet import Planet
+from src.Universe import Universe
+from src.View import View
+from src.vecN import Vec3
+
+
+FPS = 30
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 400
+
+
+def main():
+    clock = pygame.time.Clock()
+    v = View(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    planets = [
+        Planet(100, Vec3(), Vec3(0, -1), (200, 200, 200)),
+        Planet(100, Vec3(40, -10), Vec3(0, 1), (200, 0, 200)),
+        Planet(100, Vec3(100, 40), Vec3(), (0, 200, 200)),
+    ]
+
+    # # Stable circular orbit
+    # m = 1000
+    # r = 10
+    # planets = [
+    #     Planet(m, Vec3(-r), Vec3(0, 5), (200, 200, 200)),
+    #     Planet(m, Vec3(r), Vec3(0, -5), (200, 0, 200)),
+    # ]
+
+    universe = Universe(planets, 0.1, gravConst=1)
+
+    while v.running:
+        clock.tick(FPS)
+        v.drawUniverse(universe)
+        universe.stepTime()
+        v.handleEvents()
+
+
+if __name__ == '__main__':
+    main()
